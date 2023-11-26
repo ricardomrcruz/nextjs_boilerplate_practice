72afcfff426fa8d9ce8b828b16c0245ec4e8b7e7
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2023-11-26 23:53:52 +0100

    adminroutesnav

--- src/components/admin/AdminNav.tsx
+++ src/components/admin/AdminNav.tsx
@@ -1,19 +1,30 @@
 import Link from "next/link";
+import ActiveLink from "./ActiveLink";
 
 export default function AdminNav() {
+  const navLinks = [
+    { label: "Categories", href: "/admin/categories" },
+    { label: "Tags", href: "/admin/tags" },
+  ];
 
+  return (
+    <header className="h-full w-64  p-6 bg-[#363636]">
+      <Link href={"/admin"}>
+        <h1 className="text-[#ac362e] text-4xl mt-5">BackOffice</h1>
+      </Link>
 
-    return(
-
-        <header className="h-full w-64  p-6 bg-[#363636]">
-            <Link href={"/admin"}>
-                <h1 className="text-[#ac362e] text-4xl">BackOffice</h1>
-            </Link>
-
-            
-
-        </header>
-
-
-    )
+      <div className="flex flex-col mt-10">
+        {navLinks.map((l) => (
+          <ActiveLink
+            key={l.href}
+            href={l.href}
+            className="p-2 rounded text-white"
+            activeClassName="bg-slate-200"
+          >
+            {l.label}
+          </ActiveLink>
+        ))}
+      </div>
+    </header>
+  );
 }
